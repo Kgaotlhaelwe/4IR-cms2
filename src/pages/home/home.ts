135106ea8693bb88214c60872f4982879254117f
CodeName-Spaz <spazgrafficz@outlook.com>
2019-05-30 17:27:31 +0200

    Changes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -76,13 +76,13 @@ export class HomePage {
   hideSlide() {
     var blurMap = document.getElementById("map");
     // let slider = document.getElementsByClassName("absolutely") as HTMLCollectionOf<HTMLElement>;
-    let arrow = document.getElementById("myArrow");
+    // let arrow = document.getElementById("myArrow");
 
     // arrow[0].style.left = "48%";
     // arrow[0].style.transform = "translateX(-60%)";
     // arrow[0].style.transform = "rotateZ(0DEG)";
     // slider[0].style.bottom = "-180px";
-    arrow.style.transform = "rotateZ(0deg)";
+    // arrow.style.transform = "rotateZ(0deg)";
     blurMap.style.filter = "blur(0px)";
 
     this.state = 0;
