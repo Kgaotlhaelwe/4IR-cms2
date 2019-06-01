28518c32babf4d4a4b25b159e132fd7b444579df
unknown <janetzango@gmail.com>
2019-06-01 13:13:15 +0200

    changes

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -24,44 +24,7 @@ export class LoginPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
   }
-  SignIn(email: string, password: string) {
-    console.log(email, password)
-    let loading = this.loadingCtrl.create({
-      spinner: 'bubbles',
-      content: 'Signing in...',
-      duration: 40000
-    });
-    loading.present();
-    this.IRmethods.loginx(email, password).then((user: any) => {
-      // console.log(user);
-      // this.IRmethods.checkVerification().then((data: any) => {
-      //   if (data == 0) {
-      //     const alert = this.alertCtrl.create({
-      //       // title: "No Password",
-      //       subTitle: "We have sent you a verification mail, Please activate your account with the link in the mail",
-      //       buttons: ['OK'],
-      //       // cssClass: 'myAlert',
-      //     });
-      //     loading.dismiss()
-      //     alert.present();
-      //   }
-      //   else if (data == 1) {
-      //     loading.dismiss()
-      //     this.navCtrl.setRoot(HomePage);
-      //   }
-      // })
-    }).catch((error) => {
-      const alert = this.alertCtrl.create({
-        // title: "No Password",
-        subTitle: error.message,
-        buttons: ['OK'],
-        cssClass: 'myAlert',
-      });
-      loading.dismiss()
-      alert.present();
-    })
-    this.navCtrl.push(HomePage)
-  }
+
 
   goToSignIn(){
     this.navCtrl.push(RegisterPage)
