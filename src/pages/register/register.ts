9795cf15cb8548ddb55b2a76c5ffa19b76afcfd8
unknown <janetzango@gmail.com>
2019-06-03 16:22:50 +0200

    changes

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -4,6 +4,7 @@ import { IrMethodsProvider } from '../../providers/ir-methods/ir-methods';
 import { HomePage } from '../home/home';
 import { OnBoardingPage } from '../on-boarding/on-boarding';
 declare var google;
+declare var firebase;
 /**
  * Generated class for the RegisterPage page.
  *
@@ -30,7 +31,7 @@ export class RegisterPage {
   description;
   items = new Array();
   serviceArray = new Array();
-
+  
   service ;
   signUpEmail ;
   signUppassword ;
@@ -132,4 +133,86 @@ export class RegisterPage {
 
   
    }
+
+   forgotpassword(PlaceObject: object) {
+    return new Promise((resolve, reject) => {
+      if (this.email == null || this.email == undefined) {
+        const alert = this.alertCtrl.create({
+          cssClass: "myAlert",
+          title: 'Forgot your password?',
+          message: "We just need your registered email address to reset your password.",
+          
+          // cssClass: 'myAlert',
+          inputs: [
+            {
+              name: 'email',
+              placeholder: 'Your email address'
+            },
+          ],
+          buttons: [
+            {
+              text: 'Cancel',
+              handler: data => {
+                console.log('Cancel clicked');
+              }
+            },
+            {
+              text: 'Send',
+              handler: data => {
+                console.log('Saved clicked');
+
+                this.IRmethods.forgetPassword(data.email).then(()=>{
+                  console.log("forgot password works");
+                  const alert = this.alertCtrl.create({
+                    cssClass: "myAlert",
+                    title: 'Confirmation',
+                    subTitle: "Please check your email to reset your password",
+                    buttons: ['OK']
+                  });
+                  alert.present();
+                })
+              }
+            }
+          ],
+        });
+        alert.present();
+      }
+      else if (this.email != null || this.email != undefined) {
+        firebase.auth().sendPasswordResetEmail(this.email).then(() => {
+          const alert = this.alertCtrl.create({
+            cssClass: "myAlert",
+            title: 'Password request Sent',
+            subTitle: "We've sent you and email with a reset link, go to your email to recover your account.",
+            buttons: ['OK'],
+
+          });
+          alert.present();
+          resolve()
+        }, Error => {
+          const alert = this.alertCtrl.create({
+            cssClass: "myAlert",
+            subTitle: Error.message,
+            buttons: ['OK'],
+            // cssClass: 'myAlert'
+          });
+          alert.present();
+          resolve()
+        });
+      }
+    }).catch((error) => {
+      const alert = this.alertCtrl.create({
+        cssClass: "myAlert",
+        subTitle: error.message,
+        buttons: [
+          {
+            text: 'ok',
+            handler: data => {
+              console.log('Cancel clicked');
+            }
+          }
+        ],
+      });
+      alert.present();
+    })
+  }
 }
