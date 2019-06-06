7c140ef427d7371ffa8d2f4916e32ba1119293c7
AnorldMaatjie <Kgaotlhaelwe@gmail.com>
2019-06-06 15:40:30 +0200

    css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ import { ForgotpasswordPage } from '../pages/forgotpassword/forgotpassword';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage :any =  RegisterPage  ; 
+  rootPage :any =  OnBoardingPage  ; 
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen,public IRmethods: IrMethodsProvider) {
     platform.ready().then(() => {
